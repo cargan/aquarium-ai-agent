f70eab6bdb7b27489420c9cfd647b451bc38cc6f
Linas Ziedas <ziedas@gmail.com>
2026-05-13 17:06:31 +0300

    New aqua image

--- agent.py
+++ agent.py
@@ -3,7 +3,7 @@ from vision import run_local_vision_agent
 
 
 MODEL_NAME = "llama3.2-vision"
-IMAGE_PATH = "images/cVsEvlhs.jpg"
+IMAGE_PATH = "images/xUZmPdHT.jpg"
 CONTEXT_PATH = "aquarium_40_context.json"
 
 # 3. Paleidimas
