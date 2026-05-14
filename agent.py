36ebbcd7c28afe8c05fdd2013131b2d8ddb2ce6e
Linas Ziedas <ziedas@gmail.com>
2026-05-14 07:59:41 +0300

    full cycle

--- agent.py
+++ agent.py
@@ -1,16 +1,17 @@
 from storage import save_to_history, update_google_sheets
 from vision import run_local_vision_agent
+from download_photo import download_latest_photo_from_drive
 
 
 MODEL_NAME = "llama3.2-vision"
-IMAGE_PATH = "images/xUZmPdHT.jpg"
 CONTEXT_PATH = "aquarium_40_context.json"
 
-# 3. Paleidimas
 if __name__ == "__main__":
-    ai_ataskaita = run_local_vision_agent(
-        model=MODEL_NAME, image_path=IMAGE_PATH, context_path=CONTEXT_PATH
-    )
+    image = download_latest_photo_from_drive()
+    if image:
+        ai_ataskaita = run_local_vision_agent(
+            model=MODEL_NAME, image_path=f"snapshots/{image}", context_path=CONTEXT_PATH
+        )
 
-    save_to_history(ai_ataskaita)
-    update_google_sheets(ai_ataskaita)
+        save_to_history(ai_ataskaita)
+        update_google_sheets(ai_ataskaita)
